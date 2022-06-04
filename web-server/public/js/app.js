921464730700413fea793b4ba0c4d0827bd7a0f2
Jonah Saysourivong <jsaysourivong@gmailcom>
2022-06-03 23:28:04 -0700

    Created search form on weather page to fetch provided address from user

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,21 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-  response.json().then((data) => {
-    console.log(data)
-  })
-})
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//   response.json().then((data) => {
+//     console.log(data)
+//   })
+// })
 
-fetch('http://localhost:3000/weather?address=Sacramento').then((response) => {
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const location = searchElement.value
+
+  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
   response.json().then((data) => {
     if(data.error){
       console.log(data.error)
@@ -15,4 +24,5 @@ fetch('http://localhost:3000/weather?address=Sacramento').then((response) => {
       console.log(data.forecast)
     }
   })
+})
 })
