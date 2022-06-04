68cabc767b57b5c7c1d79ef0eadbd77c9336ed8a
Jonah Saysourivong <jsaysourivong@gmailcom>
2022-06-03 23:49:27 -0700

    wired up the user interface of input and button elements. 1) provided boarders. 2) added padding. 3) buttons we're provided background color, color, and cursor was set to pointer.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,27 +1,27 @@
 console.log('Client side javascript file is loaded!')
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//   response.json().then((data) => {
-//     console.log(data)
-//   })
-// })
-
-
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+//messageOne.textContent = 'From Javascript'
 
 weatherForm.addEventListener('submit', (event) => {
   event.preventDefault()
 
   const location = searchElement.value
+  
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
 
   fetch('http://localhost:3000/weather?address=' + location).then((response) => {
   response.json().then((data) => {
     if(data.error){
-      console.log(data.error)
+      messageOne.textContent = data.error
     } else {
-      console.log(data.location)
-      console.log(data.forecast)
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
     }
   })
 })
